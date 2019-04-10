dbdc5252ef7852b34a85a6ea61e2f14f8c28436c
Zhao vistart <i@vistart.me>
2019-04-10 08:50:45 +0800

    Update Marc.php
    Adjust key list.

--- models/elasticsearch/Marc.php
+++ models/elasticsearch/Marc.php
@@ -26,11 +26,10 @@ use rhoone\library\providers\huiwen\models\mongodb\MarcStatus;
  * @property string[] $ISBNs
  * @property string[] $subjects
  * @property string[] $classifications
- * @property string $abstract
+ * @property string $type
+ * @property string $status
  * @property string[][] $copies
  * @property string[] $notes
- * @property string $type
- * @property string $page_visit
  * @package rhoone\library\providers\huiwen\models\elasticsearch
  */
 class Marc extends \yii\elasticsearch\ActiveRecord
@@ -65,10 +64,15 @@ class Marc extends \yii\elasticsearch\ActiveRecord
         if (empty($titleAndAuthor)) {
             return null;
         }
+        // The "TitleAndAuthor" string is divided into several segments by "/" as a separator.
         $exploded = explode("/", $titleAndAuthor);
+
+        // If more than one result fragment is separated, all the clips except the last one are merged.
+        $counter = 1;
         while (count($exploded) > 2) {
-            $exploded[0] = $exploded[0] . $exploded[1];
-            unset($exploded[1]);
+            $exploded[0] = $exploded[0] . $exploded[$counter];
+            unset($exploded[$counter]);
+            $counter++;
         }
         return $exploded;
     }
@@ -108,9 +112,10 @@ class Marc extends \yii\elasticsearch\ActiveRecord
         $this->titles[$i]['key'] = '题名';
         $this->titles[$i]['title'] = $titleAndAuthor[0];
 
-        $keyAdditionalList = ['并列正题名', '统一题名', '翻译提名', '封面提名', '书脊题名','前题名', '后续提名',
-            '卷端题名', '变异题名', '曾用题名', '简体题名',  '丛编统一题名', '丛编题名', '作品集统一题名',
-            '逐页题名', '识别提名', '附加统一题名', '附加题名页题名', '其它题名'];
+        $keyAdditionalList = ['并列正题名', '统一题名', '翻译提名', '封面提名', '书脊题名', '前题名', '后续提名',
+            '卷端题名', '变异题名', '曾用题名(连续出版物)', '展开题名(连续出版物)', '简体题名',  '丛编统一题名',
+            '丛编题名', '作品集统一题名', '逐页题名', '识别提名(连续出版物)', '附加统一题名', '附加非控制题名',
+            '附加题名页题名', '其它题名', '先前出版物', '后续出版物', '编目员补充题名'];
         $offset = count($this->titles);
         $this->titles = array_merge($this->titles, $this->populateKeyValuePairs($marcInfos, $keyAdditionalList, $offset));
     }
@@ -121,15 +126,16 @@ class Marc extends \yii\elasticsearch\ActiveRecord
     public function setAuthors(array $marcInfos)
     {
         $this->authors = [];
+        /* Does not need to extract from it.
         $i = 0;
         $keyTitleAndAuthor = '题名/责任者';
         $titleAndAuthor = $this->extractTitleAndAuthor($this->extractValues($marcInfos, $keyTitleAndAuthor));
 
         $this->titles[$i]['key'] = '责任者';
         $this->titles[$i]['title'] = end($titleAndAuthor);
-
-        $keyAdditionalList = ['个人主要责任者', '个人次要责任者', '个人责任者', '其他责任者', '团体主要责任者',
-            '团体次要责任者', '团体责任者', '丛编个人名称', '丛编会议名称', '丛编团体名称'];
+        */
+        $keyAdditionalList = ['个人责任者', '个人主要责任者', '个人次要责任者', '团体责任者', '团体主要责任者',
+            '团体次要责任者', '丛编个人名称', '丛编团体名称', '丛编会议名称', '其他责任者'];
         $offset = count($this->authors);
         $this->authors = array_merge($this->authors, $this->populateKeyValuePairs($marcInfos, $keyAdditionalList, $offset));
     }
@@ -228,10 +234,13 @@ class Marc extends \yii\elasticsearch\ActiveRecord
     public function setNotes(array $marcInfos)
     {
         $keyAdditionalList = ['一般附注', '专款信息附注', '丛编附注', '主题检索附注', '书目附注', '传记或历史附注',
-            '使用对象附注', '内容附注', '出版发行附注', '出版周期附注', '制作者附注', '制作附注', '前提明附注',
+            '使用对象附注', '内容附注', '出版发行附注', '出版周期附注', '制作者附注', '制作附注', '前题名附注',
             '原作版本附注', '原版附注', '合订附注', '复制品附注', '学位论文附注', '引文/参考附注', '引文附注',
             '报告附注', '提要文摘附注', '摘要附注', '收藏地附注', '文献获奖附注', '时间地点附注', '标识号附注',
-            '演出者附注', '版本附注', '特殊细节附注', '相关题名附注'];
+            '演出者附注', '版本附注', '特殊细节附注', '相关题名附注', '系统细节附注', '索引文摘附注', '累积索引附注',
+            '编号特点附注', '著录信息附注', '补编附注', '表演者附注', '装订获得附注', '计算机文件附注', '计算机类型附注',
+            '计算机细节附注', '语种附注', '读者对相附注', '责任者附注', '载体形态附注', '连接字段附注', '连接款目附注',
+            '采访附注', '题名责任附注'];
         $offset = count($this->notes);
         $this->notes = array_merge($this->notes, $this->populateKeyValuePairs($marcInfos, $keyAdditionalList, $offset));
     }
@@ -273,9 +282,8 @@ class Marc extends \yii\elasticsearch\ActiveRecord
     {
         return [
             'titles',
-            'presses',
             'authors',
-            'prices',
+            'presses',
             'forms',
             'ISBNs',
             'subjects',
@@ -343,6 +351,10 @@ class Marc extends \yii\elasticsearch\ActiveRecord
                     'ISBNs' => [
                         'properties' => [
                             'ISBN' => [
+                                'type' => 'text',
+                                'fielddata' => true,
+                            ],
+                            'price' => [
                                 'type' => 'text',
                                 'fielddata' => true,
                             ],
