64e5b1dc5a9fbf4e8dc3e545dc09d8acb83c1dbc
Zhao vistart <i@vistart.me>
2019-04-28 01:34:48 +0800

    Add Extension and Library.

---
+++ Extension.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.me/
+ * @copyright Copyright (c) 2016 - 2019 vistart
+ * @license https://vistart.me/license/
+ */
+namespace rhoone\library\providers\huiwen;
+
+/**
+ * Class Extension
+ * @package rhoone\library\providers\huiwen
+ */
+abstract class Extension extends \rhoone\library\Extension
+{
+    /**
+     * @var string
+     */
+    public $libraryClass;
+}
---
+++ Library.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016-2018 vistart
+ * @license https://vistart.name/license/
+ */
+namespace rhoone\library\providers\huiwen;
+
+/**
+ * Class Library
+ * @package rhoone\library\providers\huiwen
+ */
+abstract class Library extends \rhoone\library\Library
+{
+}
